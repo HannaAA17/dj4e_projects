a38e0cef8aca0764628ffa6fcef01fc6bb73dbab
Hanna-Linux <alberto.adell17@gmail.com>
2022-07-26 16:33:50 +0200

    C2-W2 views, render and stuff

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,44 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Question
+
+# def index(request):
+#     return HttpResponse("Hello, world. aff71375 is the polls index.")
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
+
+
+# from django.template import loader
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
 
 
 def index(request):
-    return HttpResponse("Hello, world. aff71375 is the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
