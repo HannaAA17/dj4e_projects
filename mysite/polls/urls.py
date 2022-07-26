cb2d8eef763af99ee25e3d442a0da52e554b7183
Alberto Hanna <alberto.adell17@gmail.com>
2022-07-26 18:50:07 +0200

    C2W2 - Added Owner view - quiz -

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,6 +7,8 @@ app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/owner
+    path('owner', views.owner, name='owner'),
     # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -57,3 +57,7 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+def owner(request):
+    return HttpResponse("Hello, world. aff71375 is the polls index.")
