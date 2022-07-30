3cb6f7162295a95b28a57692e3674683ec74df08
Alberto Hanna <alberto.adell17@gmail.com>
2022-07-30 22:04:35 +0200

    C2-W4 generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,13 +6,13 @@ app_name = 'polls'
 
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/owner
     path('owner', views.owner, name='owner'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
