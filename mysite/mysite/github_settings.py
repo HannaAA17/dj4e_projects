da322d35ffcac7ffbcfb367ffc189a7783cbdc7b
Alberto Hanna <alberto.adell17@gmail.com>
2022-10-25 13:07:03 +0200

    C4W3 - challenges test

---
+++ mysite/mysite/github_settings.py
@@ -0,0 +1,40 @@
+
+# Copy this file to github_settings.py (don't check it into github)
+
+# Go to https://github.com/settings/developers
+
+# Add a New OAuth2 App 
+
+# Using PythonAnywhere here are some settings:
+
+# Application name: ChuckList PythonAnywhere
+# Homepage Url: https://drchuck.pythonanywhere.com
+# Application Description: Whatever
+# Authorization callback URL: https://drchuck.pythonanywhere.com/oauth/complete/github/
+
+# Also on PythonAnywhere, go into the Web tab and enable "Force HTTPS"
+# so you don't get a redirect URI mismatch.
+
+# Then copy the client_key and secret to this file
+
+SOCIAL_AUTH_GITHUB_KEY = '84ac77f6e94acbd396dd'
+SOCIAL_AUTH_GITHUB_SECRET = 'bd909ded45f792906f87eb0f7d897d918ea7e5ab'
+
+# Ask for the user's email (don't edit this line)
+SOCIAL_AUTH_GITHUB_SCOPE = ['user:email']
+
+# Note you may not get email for github users that don't make their
+# email public - that is OK
+
+# For detail: https://readthedocs.org/projects/python-social-auth/downloads/pdf/latest/
+
+# Using ngrok is hard because the url changes every time you start ngrok
+
+# If you are running on localhost, here are some settings:
+
+# Application name: ChuckList Local
+# Homepage Url: http://localhost:8000
+# Application Description: Whatever
+# Authorization callback URL: http://127.0.0.1:8000/oauth/complete/github/
+
+
